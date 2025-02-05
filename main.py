f3627e1ffe23d01d7c1f09b11ea0856f53b93c6c
Uchitoru93 <markus.rosvall@gmail.com>
2025-02-05 21:09:31 +0100

    Adding the playable object and drawing it

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
  
 
     print(f"""Starting asteroids! 
@@ -17,6 +20,7 @@ Screen height: {SCREEN_HEIGHT}""")
             if event.type == pygame.QUIT:
                 return
         screen.fill((1, 1, 1))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
