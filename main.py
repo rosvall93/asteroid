3b017d7310f5ba04e9513e20e57c7fe631a2fb24
Uchitoru93 <markus.rosvall@gmail.com>
2025-02-07 13:04:06 +0100

    Groups enables for updating and drawing

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
 
  
 
@@ -22,8 +27,9 @@ Screen height: {SCREEN_HEIGHT}""")
                 running = False
 
         screen.fill((1, 1, 1))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
